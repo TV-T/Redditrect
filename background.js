ff8536cae5003294b8a67a7e729326636415bdf1
Timothy Vane-Tempest <tim@vane-tempest.net>
2024-07-31 23:56:18 +0300

    Remove logging and inline.

--- background.js
+++ background.js
@@ -1,14 +1,11 @@
-function redirectRequest(details) {
-  console.log('Hello from background.js');
-  const url = new URL(details.url);
-  if (url.hostname.includes('reddit.com') && !url.hostname.includes('old.reddit.com')) {
-    const oldRedditUrl = new URL(`https://old.reddit.com${url.pathname}`);
-    return { redirectUrl: oldRedditUrl.toString() };
-  }
-}
-
 browser.webRequest.onBeforeRequest.addListener(
-  redirectRequest,
+  (details) => {
+    const url = new URL(details.url);
+    if (url.hostname.includes('reddit.com') && !url.hostname.includes('old.reddit.com')) {
+      const oldRedditUrl = new URL(`https://old.reddit.com${url.pathname}`);
+      return { redirectUrl: oldRedditUrl.toString() };
+    }
+  },
   { urls: ["*://*.reddit.com/*"] },
   ["blocking"]
 );
