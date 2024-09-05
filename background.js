6d668f08ddb7833e93662d0f8c71d6656dc253a6
Timothy Vane-Tempest <tim@vane-tempest.net>
2024-09-05 22:35:06 +0300

    Add toggle on/off. Scuffed icons.

--- background.js
+++ background.js
@@ -1,5 +1,26 @@
+let enabled = true;
+
+const getIcon = (enabled) => {
+  if (enabled) return {
+    path: {
+      "16": "icons/redditrect-16.png",
+      "32": "icons/redditrect-32.png",
+      "64": "icons/redditrect-64.png",
+    },
+  };
+  return {
+    path: {
+      "16": "icons/redditrect-16-grey.png",
+      "32": "icons/redditrect-32-grey.png",
+      "64": "icons/redditrect-64-grey.png",
+    },
+  };
+};
+
 browser.webRequest.onBeforeRequest.addListener(
   (details) => {
+    if (!enabled) return details.url;
+
     const url = new URL(details.url);
     if (url.hostname.includes('reddit.com') && !url.hostname.includes('old.reddit.com')) {
       const oldRedditUrl = new URL(`https://old.reddit.com${url.pathname}`);
@@ -8,4 +29,11 @@ browser.webRequest.onBeforeRequest.addListener(
   },
   { urls: ["*://*.reddit.com/*"] },
   ["blocking"]
+);
+
+browser.browserAction.onClicked.addListener(
+  () => {
+    enabled = !enabled;
+    browser.browserAction.setIcon(getIcon(enabled));
+  }
 );
